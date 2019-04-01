6f0418379c83c45ed8984e3c8df96324092df2f7
qianxuebing <2625318262@qq.com>
2019-04-01 14:25:28 +0800

    init2

---
+++ src/components/Toast.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="toast"></div>
+</template>
+
+<script>
+export default {
+  name: 'Toast'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import App from './App'
 import router from './router'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
+import './assets/css/font-awesome.css'
+import './assets/css/indexicon.css'
+import './assets/css/globle.css'
 
 Vue.config.productionTip = false
 Vue.use(MintUI)

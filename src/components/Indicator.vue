436f8f7d20c034c98f5b51cf2e9414ba93bcc2ce
qianxuebing <2625318262@qq.com>
2019-04-01 17:15:31 +0800

    indicator

---
+++ src/components/Indicator.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="indicator">
+      <gheader :examplename="examplename"></gheader>
+      <div class="room">
+        <ul>
+          <li>
+            <mt-button
+              size="large"
+              @click="indicator1">
+              Indicator1
+            </mt-button>
+          </li>
+          <li>
+            <mt-button
+              size="large"
+              @click="indicator2">
+              Indicator2
+            </mt-button>
+          </li>
+          <li>
+            <mt-button
+              size="large"
+              @click="indicator3">
+              Indicator3
+            </mt-button>
+          </li>
+        </ul>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Indicator',
+  data () {
+    return {
+      examplename: 'Indicator'
+    }
+  },
+  methods: {
+    indicator1 () {
+      this.$indicator.open()
+      setTimeout(() => {
+        this.$indicator.close()
+      }, 3000)
+    },
+    indicator2 () {
+      this.$indicator.open('加载中')
+      setTimeout(() => {
+        this.$indicator.close()
+      }, 3000)
+    },
+    indicator3 () {
+      this.$indicator.open({
+        text: '加载中...',
+        spinnerType: 'fading-circle'
+      })
+      setTimeout(() => {
+        this.$indicator.close()
+      }, 3000)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .room{
+    padding: 10px;
+    ul li{
+      margin-bottom: 20px;
+      &:first-child{
+        margin-top: 30px;
+      }
+    }
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 import GHeader from '@/components/GlobalHeader'
 import GFooter from '@/components/GlobalFooter'
 import Toast from '@/components/Toast'
+import Indicator from '@/components/Indicator'
 
 Vue.use(Router)
 Vue.component('gheader', GHeader)
@@ -13,6 +14,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Indicator',
+      component: Indicator
+    },
+    {
+      path: '/Toast',
       name: 'Toast',
       component: Toast
     }

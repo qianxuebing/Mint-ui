5e1901b2b2962dd86eb9768e07fe1995507ad2f8
qianxuebing <2625318262@qq.com>
2019-04-01 15:01:22 +0800

    Toast

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -1,13 +1,60 @@
 <template>
-  <div class="toast"></div>
+  <div class="toast">
+    <ul>
+      <li>
+        <mt-button
+          size="large"
+          @click="DefaultToast">
+          默认Toast
+        </mt-button>
+      </li>
+      <li>
+        <mt-button
+          size="large"
+          @click="IconToast">
+          带icon的Toast
+        </mt-button>
+      </li>
+      <li>
+        <mt-button
+          size="large"
+          @click="PositionToast">
+          自定义位置Toast
+        </mt-button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Toast'
+  name: 'Toast',
+  methods: {
+    DefaultToast () {
+      this.$toast('默认Toast')
+    },
+    IconToast () {
+      this.$toast({
+        message: '操作成功',
+        iconClass: 'indexicon icon-toastsuccess'
+      })
+    },
+    PositionToast () {
+      this.$toast({
+        message: '提示',
+        position: 'bottom',
+        duration: 2000
+      })
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
+  ul{
+    padding: 20px 10px 0;
+    li {
+      margin: 20px 0;
+    }
+  }
 </style>
